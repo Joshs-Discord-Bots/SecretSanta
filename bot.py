469244bc656a3f0b11e2dcf57f058eb9bcb80302
Joshuahuahua <joshuahollander2001@gmail.com>
2021-10-13 23:45:17 +0800

    Added more stuff

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ from os import system
 import random
 import json
 
+#region Init
+
 def read(readFilename):
 	try:
 		with open(readFilename) as json_file:
@@ -16,6 +18,17 @@ def write(data, writeFilename):
 	with open(writeFilename, 'w') as outfile:
 		json.dump(data, outfile, indent=4)
 	return
+
+def convertDictionaryKeyFromStringToInteger(oldDict):
+	newDict = {}
+	for oldKey in oldDict:
+		if isinstance(oldKey, str):
+			newKey = int(oldKey)
+			newDict[newKey] = oldDict[oldKey]
+		else:
+			newDict[oldKey] = oldDict[oldKey]
+	return newDict
+
 
 if not os.path.isfile('config.json'):
 	def_config = {
@@ -34,10 +47,9 @@ intents.messages = config['intents']['messages']
 intents.members = config['intents']['members']
 intents.guilds = config['intents']['guilds']
 
-prefix = config['prefix']
 
-activity = discord.Game(name=f"{prefix}help")
-bot = commands.Bot(command_prefix = prefix, intents=intents, activity=activity, status=discord.Status.online, case_insensitive=True)
+activity = discord.Game(name=f"{config['prefix']}help")
+bot = commands.Bot(command_prefix = config['prefix'], intents=intents, activity=activity, status=discord.Status.online, case_insensitive=True)
 bot.remove_command('help')
 
 bot.token = config['token']
@@ -52,12 +64,13 @@ def admin(user):
 system('cls')
 print('Booting Up...')
 
-
+#endregion
 
 @bot.event
 async def on_ready():
 	system('cls')
 	print(f'{bot.user} has connected to Discord!')
+
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -65,7 +78,6 @@ async def on_command_error(ctx, error):
 		await ctx.send('That user does not exist!')
 	elif isinstance(error, commands.MissingPermissions):
 		await ctx.send('You are not allowed to do that!')
-
 
 @bot.command()
 async def join(ctx, user: discord.Member = None):
@@ -76,7 +88,7 @@ async def join(ctx, user: discord.Member = None):
 	else:
 		user = ctx.author
 	
-	niceList = read(filename)
+	niceList = convertDictionaryKeyFromStringToInteger(read(filename))
 	if niceList == None:
 		niceList = {}
 	if ctx.guild.id not in niceList or niceList[ctx.guild.id]['unshuffled'] == []:
@@ -90,16 +102,18 @@ async def join(ctx, user: discord.Member = None):
 	write(niceList, filename)
 
 @bot.command()
-async def prefix(ctx, char = None):
-	if not admin(ctx):
+async def setPrefix(ctx, char = None):
+	if not admin(ctx.author):
 		await ctx.send('You are not allowed to do that!')
 	elif char == None:
-		await ctx.send(f'Usage: `{prefix}prefix <char>`')
+		await ctx.send(f'Usage: `{bot.command_prefix}setPrefix <char>`')
 		return
 	else:
 		config['prefix'] = char[0]
 		write(config, 'config.json')
-
+		bot.command_prefix = char[0]
+		await bot.change_presence(activity=discord.Game(name=f"{bot.command_prefix}help"))
+		await ctx.send(f'Bot command prefix changed to `{bot.command_prefix}`')
 
 @bot.command()
 async def remove(ctx, user: discord.Member = None):
@@ -108,10 +122,10 @@ async def remove(ctx, user: discord.Member = None):
 		return
 
 	if user == None:
-		await ctx.send(f'Usage: `{prefix}remove <user>`')
+		await ctx.send(f'Usage: `{bot.command_prefix}remove <user>`')
 		return
 		
-	niceList = read(filename)
+	niceList = convertDictionaryKeyFromStringToInteger(read(filename))
 	if niceList == None or ctx.guild.id not in niceList or user.id not in niceList[ctx.guild.id]['unshuffled']:
 		await ctx.send(f'{user.name} is not on the list!')
 	else:
@@ -121,10 +135,22 @@ async def remove(ctx, user: discord.Member = None):
 	write(niceList, filename)
 
 
+@bot.command()
+async def leave(ctx):
+	user = ctx.author
+	niceList = convertDictionaryKeyFromStringToInteger(read(filename))
+	if niceList == None or ctx.guild.id not in niceList or user.id not in niceList[ctx.guild.id]['unshuffled']:
+		await ctx.send('You are not on the list!')
+	else:
+		niceList[ctx.guild.id]['unshuffled'].remove(user.id)
+		niceList[ctx.guild.id]['shuffled'].remove(user.id)
+		await ctx.send('You have been taken off the list!')
+	write(niceList, filename)
+
 
 @bot.command()
 async def list(ctx):
-	niceList = read(filename)
+	niceList = convertDictionaryKeyFromStringToInteger(read(filename))
 	embed = discord.Embed ( # Message
 		title='Secret Santa List',
 		colour=discord.Colour.green()
@@ -139,15 +165,13 @@ async def list(ctx):
 		embed.description = list
 	await ctx.send(embed=embed)
 
-
-
 @bot.command()
 async def clear(ctx):
 	if not admin(ctx.author):
 		await ctx.send('You are not allowed to do that!')
 		return
 
-	niceList = read(filename)
+	niceList = convertDictionaryKeyFromStringToInteger(read(filename))
 	if niceList == None or ctx.guild.id not in niceList or len(niceList[ctx.guild.id]['unshuffled']) == 0:
 		await ctx.send('List is already empty!')
 		return
@@ -169,14 +193,13 @@ async def clear(ctx):
 	write(niceList, filename)
 	await ctx.send('The list has now been cleared!')
 
-
 @bot.command()
 async def shuffle(ctx, view = ''):
 	if not admin(ctx.author):
 		await ctx.send('You are not allowed to do that!')
 		return
 
-	niceList = read(filename)
+	niceList = convertDictionaryKeyFromStringToInteger(read(filename))
 	if niceList == None or ctx.guild.id not in niceList or len(niceList[ctx.guild.id]['unshuffled']) == 0:
 		await ctx.send('Cant view/shuffle an empty list!')
 		return
@@ -187,7 +210,7 @@ async def shuffle(ctx, view = ''):
 		random.shuffle(shuffledList)
 		niceList[ctx.guild.id]['shuffled'] = shuffledList
 		write(niceList, filename)
-		await ctx.send(f'List has been shuffled!\nView it with `{prefix}shuffle view`')
+		await ctx.send(f'List has been shuffled!\nView it with `{bot.command_prefix}shuffle view`')
 	elif view.lower() == 'view':
 		await ctx.send('DMing shuffled list...')
 		embed = discord.Embed (
@@ -200,9 +223,7 @@ async def shuffle(ctx, view = ''):
 			embed.add_field(name=curr.name, value=pair.name, inline=False)
 		await ctx.author.send(embed=embed)
 	else:
-		await ctx.send(f'Usage: `{prefix}shuffle [view]`')
-
-
+		await ctx.send(f'Usage: `{bot.command_prefix}shuffle [view]`')
 
 @bot.command()
 async def start(ctx):
@@ -210,7 +231,7 @@ async def start(ctx):
 		await ctx.send('You are not allowed to do that!')
 		return
 
-	niceList = read(filename) # Check if enough people on list
+	niceList = convertDictionaryKeyFromStringToInteger(read(filename)) # Check if enough people on list
 	if niceList == None or ctx.guild.id not in niceList or len(niceList[ctx.guild.id]['unshuffled']) < 3:
 		await ctx.send('You need at least 3 people on the list!')
 		return
@@ -229,14 +250,15 @@ async def start(ctx):
 
 	shuffledList = niceList[ctx.guild.id]['shuffled']
 	for i, userid in enumerate(shuffledList):
-		curr = await bot.fetch_user(shuffledList[i])
-		pair = await bot.fetch_user(shuffledList[(i+1) % len(shuffledList)])
+		curr = ctx.guild.get_member(shuffledList[i])
+		pair = ctx.guild.get_member(shuffledList[(i+1) % len(shuffledList)])
 		# await ctx.send(f'{curr.name} -> {pair.name}')
 		if pair.nick == None:
 			await curr.send(f'Ho Ho Ho!\nYour secret santa recipient is {pair.name}!')
 		else:
 			await curr.send(f'Ho Ho Ho!\nYour secret santa recipient is {pair.nick}! ({pair.name})')
-
+	await ctx.send('Messages sent! Ho ho ho')
+	
 
 @bot.command()
 async def admins(ctx, action='list', user: discord.Member = None):
@@ -252,7 +274,7 @@ async def admins(ctx, action='list', user: discord.Member = None):
 		embed.description = list
 		await ctx.send(embed=embed)
 		return
-	elif not admin(ctx):
+	elif not admin(ctx.author):
 		await ctx.send('You do not have permission to do that!')
 		return
 	
@@ -272,9 +294,10 @@ async def admins(ctx, action='list', user: discord.Member = None):
 		else:
 			config['admins'].remove(user.id)
 			write(config, 'config.json')
-			await ctx.send(f'"{user.name}" is now an admin')
+			await ctx.send(f'"{user.name}" is no longer an admin')
 	else:
-		await ctx.send(f'Usage: `{prefix}admin add/remove <user>`')
+		await ctx.send(f'Usage: `{bot.command_prefix} admin add/remove <user>`')
+
 
 
 @bot.command()
@@ -290,8 +313,6 @@ async def reload(ctx):
 	else:
 		await ctx.send('You do not have permission to do that!')
 
-
-
 @bot.command()
 async def help(ctx):
 	embed = discord.Embed ( # Message
@@ -300,6 +321,7 @@ async def help(ctx):
 	)
 	embed.add_field(name='join', value='Add yourself to the list', inline=False)
 	embed.add_field(name='join <user>', value='Add a specified user to the list\n(admin only)', inline=False)
+	embed.add_field(name='leave', value='Remove yourself from the list', inline=False)
 	embed.add_field(name='remove <user>', value='Removes a user to the list\n(admin only)', inline=False)
 	embed.add_field(name='list', value='Lists everyone currently on the list', inline=False)
 	embed.add_field(name='clear', value='Clears the current secret santa list\n(admin only)', inline=False)
@@ -311,7 +333,7 @@ async def help(ctx):
 	embed.add_field(name='admins add <user>', value='Give user access to admin commands\n(admin only)', inline=False)
 	embed.add_field(name='admins remove <user>', value='Revoke user\'s access to admin commands\n(admin only)', inline=False)
 	
-	embed.add_field(name='prefix <char>', value='Change bot\'s prefix\n(admin only)', inline=False)
+	embed.add_field(name='setPrefix <char>', value='Change bot\'s prefix\n(admin only)', inline=False)
 	await ctx.send(embed=embed)
 
 
